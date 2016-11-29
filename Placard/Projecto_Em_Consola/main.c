d9ee99bc8d4d503fed8dfa3825241ac6181cfc53
Pedro Silva <pmiguelfs@gmail.com>
2016-11-29 20:24:52 +0000

    Adicionei o Menu 1.0

--- Placard/Projecto_Em_Consola/main.c
+++ Placard/Projecto_Em_Consola/main.c
@@ -13,6 +13,35 @@ typedef struct
 
 int main()
 {
-	int fixit;
+	int tecla_menu;
+
+	do
+	{
+		printf("OPCAO:\n");
+		printf("\t1- VER O SEU SALDO\n");
+		printf("\t2- JOGAR\n");
+		printf("\t3- LISTAR\n");
+		printf("\t4- ALTERAR DEFINICOES\n");
+		printf("\t5- GRAVAR\n");
+		printf("\t0- SAIR\n");
+		scanf("%d", tecla_menu);
+
+		switch (tecla_menu)
+		{
+		case 1:
+			escolhemodalidade();
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		default:
+			break;
+		}
+	} while (tecla_menu != 0);
 	return 0;
 }
