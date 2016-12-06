08bc45a7a982915531aed08ec8b98016e872f176
Pedro Silva <pmiguelfs@gmail.com>
2016-12-06 15:56:23 +0000

    PlacardStructs Fixing

---
+++ Placard/Projecto_Em_Consola/PlacardStructs.h
@@ -0,0 +1,16 @@
+/*HEADER FILE COM AS STRUCTS NECESSARIAS PARA TODO O CODIGO DO PROJETO*/
+
+#define n 18
+
+typedef	struct
+{
+	char nome[60];
+}clube;
+
+typedef struct
+{
+	char nome[60];
+	clube clubes[n];
+}modalidade;
+
+//bb
