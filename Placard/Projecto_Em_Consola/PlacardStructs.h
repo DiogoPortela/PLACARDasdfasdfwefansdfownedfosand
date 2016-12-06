5b3082d7e107f52ada63b46ab7b1f2d519911364
Pedro Silva <pmiguelfs@gmail.com>
2016-12-06 17:12:10 +0000

    ignore

--- Placard/Projecto_Em_Consola/PlacardStructs.h
+++ Placard/Projecto_Em_Consola/PlacardStructs.h
@@ -1,13 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define n 18
+#define n 60
 
 typedef	struct
 {
-	char nome[60];
+	char nome[n];
 }clube;
 
 typedef struct
 {
-	char nome[60];
+	char nome[n];
 	clube clubes[n];
+}evento;
+
+typedef struct
+{
+	char nome[n];
+	clube evento[n];
 }modalidade;
