1e6b78a621c7f47e83b578750ad969aa0484225d
Pedro Silva <pmiguelfs@gmail.com>
2016-12-18 20:33:20 +0000

    criajogo done

--- Placard/Projecto_Em_Consola/PlacardFunctions.c
+++ Placard/Projecto_Em_Consola/PlacardFunctions.c
@@ -186,63 +186,31 @@ void CriaJogo(modalidade mod, int a, int b) //tera de ser alterado para cada eve
 	fread = fopen("clubes.txt", "r");
 	FILE *fwrite;
 	fwrite = fopen("jogos.txt", "a");
-	char aux[60];
+	clube aux1, aux2;
+	int idaux;
 	//para o futebol e basquetebol assumi que funciona como na realidade, 2 equipas so podem jogar 2 vezes numa temporada, uma x na casa de cada 1
 	if (FicheiroExiste("clubes.txt", &fread) && FicheiroExiste("jogos.txt", &fwrite))
 	{
-			while (fscanf(fwrite, "%s - %s\n", aux) != EOF)
+		if (mod.nome != "TENIS")
+		{
+			while (fscanf(fwrite, "%d- %s - %s\n", idaux, aux1.nome, aux2.nome) != EOF)
 			{
-				if(aux == "%*d- %s - %s", mod.clube[a].nome, mod.clube[b].nome)
+				if (idaux == mod.indentificador && strcmp(aux1.nome, mod.clube[a].nome) && strcmp(aux2.nome, mod.clube[b].nome))
 				{
 					printf("AS DUAS EQUIPAS JÁ JOGARAM EM CASA DO %s", mod.clube[a].nome);
 				}
-			}
-			else
-			{
-				fprintf(fd, "%s - %s\n", casa.nome, fora.nome);
+				else
+				{
+					fprintf(fwrite, "%d- %s - %s\n", mod.indentificador, mod.clube[a].nome, mod.clube[b].nome);
+				}
 			}
 		}
-	}
-}
-
-if (mod.nome == "BASQUETEBOL")
-{
-	fd = fopen("jogosbasquetebol.txt", "a");
-
-	if (fd == NULL)
-	{
-		printf("\nFICHEIRO NAO ENCONTRADO!");
-	}
-	else
-	{
-		while (fscanf("%s - %s\n", aux1.nome, aux2.nome) != EOF)
+		//para o tenis assumi que podes jogar varias vezes com a mesma pessoa (cria sempre o jogo)
+		else
 		{
-			if (strcmp(aux1.nome, casa.nome) && strcmp(aux2.nome, fora.nome))
-			{
-				printf("AS DUAS EQUIPAS JÁ JOGARAM EM CASA DO %S", casa.nome);
-			}
-			else
-			{
-				fprintf(fd, "%s - %s\n", casa.nome, fora.nome);
-			}
+			fprintf(fwrite, "%d- %s - %s\n", mod.indentificador, mod.clube[a].nome, mod.clube[b].nome);
 		}
 	}
+	fclose(fread); fclose(fwrite);
 }
-//para o tenis assumi que podes jogar varias vezes com a mesma pessoa (cria sempre o jogo)
-if (mod.nome == "TENIS")
-{
-	fd = fopen("jogostenis.txt", "a");
 
-	if (fd == NULL)
-	{
-		printf("\nFICHEIRO NAO ENCONTRADO!");
-	}
-	else
-	{
-		while (fscanf("%s - %s\n", aux1.nome, aux2.nome) != EOF)
-		{
-			fprintf(fd, "%s - %s\n", casa.nome, fora.nome);
-		}
-	}
-}
-}
